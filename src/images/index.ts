60c0956b1583aa9e87689302014c85db7307c920
lordscoba <lordscoba2tm@gmail.com>
2023-07-18 16:11:56 -0700

    added routing

---
+++ src/images/index.ts
@@ -0,0 +1,2 @@
+export { default as Background } from "./backgound.png";
+export { default as Background2 } from "./backgound2.png";
---
+++ src/screens/Home.tsx
@@ -0,0 +1,14 @@
+import { Hero } from "../components/home";
+import { Footer, Nav } from "../components/layout";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
